7f8e994520e36f98481b79a6aa241948d552d1a2
Bobby Kiaie <bobbykiaie@gmail.com>
2023-11-19 21:48:15 -0800

    Initial Commit

---
+++ Restaurants/RestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsApp.swift
+//  Restaurants
+//
+//  Created by Babak Kiaie on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
